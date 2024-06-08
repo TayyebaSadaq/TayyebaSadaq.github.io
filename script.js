a68f6dec6c9174a0d314fa5f990a35b8d21cedcc
TayyebaSadaq <official.tayyeba@gmail.com>
2024-06-08 18:24:23 +0100

    fixed music player and added certificates onto the website

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var player = document.getElementById("player");
 let progress = document.getElementById("progress");
 let playbtn = document.getElementById("playbtn");
+let prevbtn = document.getElementById("prevbtn"); // Get the previous button element
+let nextbtn = document.getElementById("nextbtn"); // Get the next button element
 let current = document.getElementById("current");
 var playerContainer = document.querySelector(".player");
 
@@ -15,6 +17,8 @@ var playpause = function () {
 }
 
 playbtn.addEventListener("click", playpause);
+prevbtn.addEventListener("click", playPreviousTrack); // Add event listener for previous button
+nextbtn.addEventListener("click", playNextTrack); // Add event listener for next button
 
 player.onplay = function () {
   playbtn.classList.remove("fa-play");
@@ -65,20 +69,28 @@ const playlist = [
 
 // Next track functionality
 function playNextTrack() {
-    currentTrackIndex++;
-    if (currentTrackIndex >= playlist.length) {
-        currentTrackIndex = 0; // Loop back to the first track if at the end of the playlist
-    }
-    player.src = playlist[currentTrackIndex].src;
-    player.play();
+  currentTrackIndex++;
+  if (currentTrackIndex >= playlist.length) {
+      currentTrackIndex = 0; // Loop back to the first track if at the end of the playlist
+  }
+  player.src = playlist[currentTrackIndex].src;
+  player.play();
+  updateTrackTitle(); // Update the track title
 }
 
 // Previous track functionality
 function playPreviousTrack() {
-    currentTrackIndex--;
-    if (currentTrackIndex < 0) {
-        currentTrackIndex = playlist.length - 1; // Go to the last track if at the beginning of the playlist
-    }
-    player.src = playlist[currentTrackIndex].src;
-    player.play();
+  currentTrackIndex--;
+  if (currentTrackIndex < 0) {
+      currentTrackIndex = playlist.length - 1; // Go to the last track if at the beginning of the playlist
+  }
+  player.src = playlist[currentTrackIndex].src;
+  player.play();
+  updateTrackTitle(); // Update the track title
+}
+
+// Function to update the track title
+function updateTrackTitle() {
+  let trackTitle = document.querySelector(".track-title");
+  trackTitle.textContent = playlist[currentTrackIndex].title;
 }
