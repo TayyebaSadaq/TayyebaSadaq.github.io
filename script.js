4e6d7674ec9664e67c60131b2e4e9ae7503f0264
TayyebaSadaq <official.tayyeba@gmail.com>
2024-10-27 17:29:53 +0000

    basic website complete with content and styling

---
+++ script.js
@@ -0,0 +1,24 @@
+function openModal(title, description, language, repoLink) {
+    document.getElementById('modal-title').innerText = title;
+    document.getElementById('modal-description').innerHTML = description + language;
+
+    const modalLinks = document.getElementById('modal-links');
+    modalLinks.innerHTML = ''; 
+    const li = document.createElement('li');
+    li.innerHTML = `<a href="${repoLink}" target="_blank">View Repository</a>`;
+    modalLinks.appendChild(li); 
+
+    document.getElementById('project-modal').style.display = 'block';
+}
+
+
+function closeModal() {
+    document.getElementById("project-modal").style.display = "none";
+}
+
+window.onclick = function(event) {
+    const modal = document.getElementById("project-modal");
+    if (event.target === modal) {
+        closeModal();
+    }
+};
